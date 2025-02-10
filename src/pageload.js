8283b563dab3e1aac51995d375461067f97a835a
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-02-10 15:54:00 -0600

    Add image to homepage

--- src/pageload.js
+++ src/pageload.js
@@ -2,7 +2,7 @@
 
 function homePageLoad(div) {
     // div.innerHTML = '';
-    div.innerHTML = `<img src="" alt="A large grizzly bear">
+    div.innerHTML = `<img src="https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Brown bear sitting on grass">
         <h1>Grizzly Grillhouse</h1>
         <h2>In Mother Russia, the steak eats you</h2>`;
 };
