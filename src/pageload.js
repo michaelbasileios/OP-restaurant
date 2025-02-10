0e7623cf70760d8d62f806452aa4479f83366173
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-02-10 17:46:06 -0600

    Add image credit to footer

--- src/pageload.js
+++ src/pageload.js
@@ -1,10 +1,11 @@
-// const contentDiv = document.querySelector("#content");
+const footer = document.querySelector("footer");
 
 function homePageLoad(div) {
     // div.innerHTML = '';
     div.innerHTML = `<img src="https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Brown bear sitting on grass">
         <h1>Grizzly Grillhouse</h1>
         <h2>In Mother Russia, the steak eats you</h2>`;
+    footer.innerHTML = `<p><b>Image</b>: <i>Lonely Bear</i> by Mark Basarab, via <a href="https://unsplash.com/">unsplash.com</a><p>`
 };
 
 export { homePageLoad };
