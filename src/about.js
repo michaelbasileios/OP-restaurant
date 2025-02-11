458766c792c6b407fa508ff3a56d19f14ab0abb1
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-02-11 15:32:57 -0600

    Change about page js from innerHTML to append

--- src/about.js
+++ src/about.js
@@ -1,8 +1,11 @@
 function aboutPageLoad(div){
-    // div.innerHTML = '';
-    div.innerHTML = `<h2>We are a nice cozy restaurant situated right next to 
-    the infamous Ace City Chemicals in Gotham City. Come on over and have a laugh
-    while feasting on our scrumptious irradiated meals!</h2>`
+    const divContent = document.createElement('div');
+
+    const aboutInfo = document.createElement('h2');
+    aboutInfo.textContent = "We are a nice cozy restaurant situated right next to the infamous Ace City Chemicals in Gotham City. Come on over and have a laugh while feasting on our scrumptious irradiated meals!";
+    divContent.appendChild(aboutInfo);
+
+    div.appendChild(divContent);
 }
 
 export { aboutPageLoad }
