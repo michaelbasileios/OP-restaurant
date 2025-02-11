e935a59324e0cf2c3f72c0b5996ea076e014dd1a
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-02-11 12:16:44 -0600

    Change homepage load javascript from innerHTML to append

--- src/pageload.js
+++ src/pageload.js
@@ -1,11 +1,48 @@
-const footer = document.querySelector("footer");
-
 function homePageLoad(div) {
-    // div.innerHTML = '';
-    div.innerHTML = `<img src="https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Brown bear sitting on grass">
-        <h1>Grizzly Grillhouse</h1>
-        <h2>In Mother Russia, the steak eats you</h2>`;
-    footer.innerHTML = `<p><b>Image</b>: <i>Lonely Bear</i> by Mark Basarab, via <a href="https://unsplash.com/">unsplash.com</a><p>`
+    const footer = document.querySelector("footer");
+    const divContent = document.createElement('div');
+    //create, append image
+    const image = document.createElement('img');
+    image.id = 'homeImage';
+    image.src = "https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    image.alt = "Brown bear sitting on grass";
+    divContent.appendChild(image);
+
+    //create, append heading
+    const headingOne = document.createElement('h1');
+    headingOne.textContent = "Grizzly Grillhouse";
+    divContent.appendChild(headingOne);
+
+    //create, append subheading
+    const subHeading = document.createElement('h2');
+    subHeading.textContent = "In Mother Russia, the steak eats you";
+    divContent.appendChild(subHeading);
+    
+    //append all to contentDiv
+    div.appendChild(divContent);
+
+    //create, append footer content
+    const footerText = document.createElement('p');
+    const imageSpan = document.createElement('span');
+    imageSpan.textContent = "Image: ";
+    imageSpan.style.fontWeight = 'bold';
+    footerText.appendChild(imageSpan);
+
+    const imgNameSpan = document.createElement('span');
+    imgNameSpan.textContent = "Lonely Bear ";
+    imgNameSpan.style.fontStyle = 'italic';
+    footerText.appendChild(imgNameSpan);
+
+    const imgAuthor = document.createElement('span');
+    imgAuthor.textContent = "by Mark Basarab, via ";
+    footerText.appendChild(imgAuthor);
+
+    const footerLink = document.createElement('a');
+    footerLink.href = "https://unsplash.com/";
+    footerLink.textContent = "unsplash.com";
+    footerText.appendChild(footerLink);
+
+    footer.appendChild(footerText);
 };
 
 export { homePageLoad };
